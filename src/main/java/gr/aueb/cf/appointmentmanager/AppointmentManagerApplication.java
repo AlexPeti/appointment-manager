0079d4e4ad90afa9235354dc16fba4d5f891f546
AlexPeti <alexandros.peteinaris@hotmail.com>
2023-04-24 20:24:11 +0300

    First commit

---
+++ src/main/java/gr/aueb/cf/appointmentmanager/AppointmentManagerApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.appointmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentManagerApplication.class, args);
+    }
+
+}
