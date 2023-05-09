101bb56533c5eedda9268ecbc3f0a7cbf0ec8626
AlexPeti <alexandros.peteinaris@hotmail.com>
2023-05-09 22:28:38 +0300

    Added a button for creating a new doctor in the dashboard and also created a JavaScript script to make the input fields visible when you click on the create a new doctor button

--- src/main/java/gr/aueb/cf/appointmentmanager/controller/DashboardController.java
+++ src/main/java/gr/aueb/cf/appointmentmanager/controller/DashboardController.java
@@ -1,11 +1,14 @@
 package gr.aueb.cf.appointmentmanager.controller;
 
+import gr.aueb.cf.appointmentmanager.dto.DoctorDTO;
 import gr.aueb.cf.appointmentmanager.model.Doctor;
 import gr.aueb.cf.appointmentmanager.service.IDoctorService;
 import gr.aueb.cf.appointmentmanager.service.exceptions.EntityNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,4 +27,16 @@ public class DashboardController {
         model.addAttribute("doctors", doctors);
         return "dashboard";
     }
+
+    @PostMapping("/doctors/create")
+    public String createDoctor(@RequestParam String firstname, @RequestParam String lastname) {
+        DoctorDTO doctorDTO = new DoctorDTO();
+        doctorDTO.setFirstname(firstname);
+        doctorDTO.setLastname(lastname);
+
+        doctorService.createDoctor(doctorDTO);
+
+        return "redirect:/dashboard";
+    }
+
 }
