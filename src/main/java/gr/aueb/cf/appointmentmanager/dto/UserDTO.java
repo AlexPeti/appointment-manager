5191b369d634eaa84301ed40b1b008a6f2ec6a33
AlexPeti <alexandros.peteinaris@hotmail.com>
2023-05-15 20:26:43 +0300

    Removed validation from userDTO class since we have implemented the user validator class and all validation checks are performed there

--- src/main/java/gr/aueb/cf/appointmentmanager/dto/UserDTO.java
+++ src/main/java/gr/aueb/cf/appointmentmanager/dto/UserDTO.java
@@ -4,8 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
@@ -13,13 +11,8 @@ public class UserDTO {
 
     private Long id;
 
-    @NotEmpty(message = "Field username cannot be empty")
     private String username;
 
-    @Size(min = 8, message = "Password must have at least ${min} characters")
-    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\\d).*$",
-            message = "Password must contain at least one uppercase letter, one lowercase letter, and one digit")
-    @NotEmpty(message = "Field password cannot be empty")
     private String password;
 
     @NotEmpty
